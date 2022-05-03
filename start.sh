67f53042fdac7dc995757d11b4e86420fc994608
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-03 11:41:17 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/MuhaKn/LUCIFER.git /messi
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /messi
+fi
+cd /messi
+pip3 install -U -r requirements.txt
+echo "bot start ആയിട്ടോ 😁"
+python3 bot.py
