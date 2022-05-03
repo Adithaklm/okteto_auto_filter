a99f3fb346156d65cf93a1f6004210d0db46861b
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-05-03 14:19:35 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,13 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('💠 SΞARCH HΞRΞ 💠', switch_inline_query_current_chat='')
-            ],[
-            InlineKeyboardButton('💠 GᏒԾUᎮ 💠', url='https://t.me/Film_housc'),
-            InlineKeyboardButton('💠 CHAИИΞL 💠', url='https://t.me/film_hous')
-            ],[
-            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
-            InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
+            InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
