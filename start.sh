f1599ec9b5a8b7f7d3b0e60ee8347db23155cd00
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-05-03 11:41:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MuhaKn/LUCIFER.git /messi
+  git clone https://github.com/Adithaklm/messi.git /messi
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /messi
